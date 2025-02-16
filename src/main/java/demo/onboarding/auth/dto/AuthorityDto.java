3aa6bb7940d0d52d934c788dc4dca7dd17ef7d66
JIMINJI1 <mj013031@gmail.com>
2025-02-16 16:18:33 +0900

    feat: 회원가입 dto 구현 #5

---
+++ src/main/java/demo/onboarding/auth/dto/AuthorityDto.java
@@ -0,0 +1,10 @@
+package demo.onboarding.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class AuthorityDto {
+    private String authorityName;
+}
