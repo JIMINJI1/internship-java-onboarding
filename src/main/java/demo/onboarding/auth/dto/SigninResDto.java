a3d011705e964eda0acdaa2501066f8af29b381f
JIMINJI1 <mj013031@gmail.com>
2025-02-16 16:36:40 +0900

    feat: 로그인 dto 구현 #7

---
+++ src/main/java/demo/onboarding/auth/dto/SigninResDto.java
@@ -0,0 +1,10 @@
+package demo.onboarding.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SigninResDto {
+    private String token;
+}
