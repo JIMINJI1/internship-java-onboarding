dbf75b73d6d7291a41119b26515a856e89645164
JIMINJI1 <mj013031@gmail.com>
2025-02-16 16:38:31 +0900

    feat: 로그인 Service 구현 #7

--- src/main/java/demo/onboarding/auth/service/AuthService.java
+++ src/main/java/demo/onboarding/auth/service/AuthService.java
@@ -1,8 +1,9 @@
 package demo.onboarding.auth.service;
 
-import demo.onboarding.auth.dto.AuthorityDto;
-import demo.onboarding.auth.dto.SignupReqDto;
-import demo.onboarding.auth.dto.SignupResDto;
+import demo.onboarding.auth.dto.*;
+import demo.onboarding.common.jwt.JwtUtil;
+import demo.onboarding.common.jwt.RefreshToken;
+import demo.onboarding.common.jwt.RefreshTokenRepository;
 import demo.onboarding.user.User;
 import demo.onboarding.user.UserRepository;
 import demo.onboarding.user.UserRole;
@@ -17,6 +18,8 @@ import java.util.List;
 public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     public SignupResDto signup(SignupReqDto signupReqDto) {
         String encodedPassword = passwordEncoder.encode(signupReqDto.getPassword());
@@ -31,5 +34,22 @@ public class AuthService {
         userRepository.save(user);
         List<AuthorityDto> authorities = List.of(new AuthorityDto(user.getUserRole().getAuthority()));
         return new SignupResDto(user.getUsername(), user.getNickname(), authorities);
+    }
+
+    public SigninResDto signin(SigninReqDto signinReqDto) {
+        User user = userRepository.findByUsername(signinReqDto.getUsername()).orElseThrow(
+                () -> new IllegalArgumentException("가입되지 않은 유저입니다."));
+
+        if (!passwordEncoder.matches(signinReqDto.getPassword(), user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        String accessToken = jwtUtil.generateAccessToken(user.getUsername()).substring(7);
+        String refreshToken = jwtUtil.generateRefreshToken(user.getUsername()).substring(7);
+
+        RefreshToken savedRefreshToken = new RefreshToken(user.getId().toString(), refreshToken, user.getUsername());
+        refreshTokenRepository.save(savedRefreshToken);
+
+        return new SigninResDto(accessToken);
     }
 }
